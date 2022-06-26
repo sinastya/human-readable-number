df830cddd8a1d37d632063e43cff6d7f3bb91d92
sinastya <sinastya@yandex.ru>
2022-06-26 14:58:09 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
-  
+  let result = (number == 0) ? 'zero' : '';
+  let num = number;
+  let array_0_19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let array_20_90 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  if (number >= 100) {
+    result = `${array_0_19[Math.floor(number/100)]} hundred `
+    num = number%100;
+  }
+  if (num < 20) {
+    result += array_0_19[num]
+  } else if (num >= 20 && num < 100) {
+    result += `${array_20_90[Math.floor(num/10)]} ${array_0_19[num % 10]}`
+  } 
+  return result.trim();
 }
